7514353dd5d2471861b90822bcb07b9c80fd9cda
Sofia <sofiakucmida@gmail.com>
2026-06-16 20:01:48 +0300

    added markup and logic, styles

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,1 +1,19 @@
-export {};
+import { nextServer } from './api';
+import { User } from '@/app/types/user';
+
+export type UserRegisterProps = {
+  email: string;
+  password: string;
+  username: string;
+};
+
+type RegisterResponse = {
+  newUser: User;
+};
+
+export const register = async (
+  data: UserRegisterProps,
+): Promise<RegisterResponse> => {
+  const res = await nextServer.post('/auth/register', data);
+  return res.data;
+};
